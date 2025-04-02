e599c56166dd88412480149be4b83c97d93c490f
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-04-02 00:35:47 -0400

    fix ci

--- bin/pywf_open_source/define_02_venv.py
+++ bin/pywf_open_source/define_02_venv.py
@@ -98,7 +98,7 @@ class PyWfVenv:
         self: "PyWf",
         real_run: bool = True,
         quiet: bool = False,
-    ) -> bool:  # pragma: no cover
+    ) -> bool:
         """
         Run:
 
@@ -127,7 +127,7 @@ class PyWfVenv:
         self: "PyWf",
         real_run: bool = True,
         verbose: bool = True,
-    ):  # pragma: no cover
+    ):
         """
         See :meth:`_remove_virtualenv`.
         """
